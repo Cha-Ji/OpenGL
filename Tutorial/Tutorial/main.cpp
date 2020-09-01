33991d93938a4e544353b6982cc745061c897328
차지원 <chajiwon100785@gmail.com>
2020-09-01 11:25:58 +0900

    1주차 [과제]

--- Tutorial/Tutorial/main.cpp
+++ Tutorial/Tutorial/main.cpp
@@ -1,12 +1,17 @@
-#include <GLUT/GLUT.h>
+#include <GLUT/GLUT.h> //MacOS : Xcode에서 실행해 표현이 다릅니다.
 
 void mydisplay(){
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-    glVertex3f(-0.5, -0.5, 0.0);
-    glVertex3f(0.5, -0.5, 0.0);
-    glVertex3f(0.5, 0.5, 0.0);
-    glVertex3f(-0.5, 0.5, 0.0);
+    glVertex3f(0.0, 0.9, 0.0);     // 머리
+    glVertex3f(-0.9, 0.5, 0.0);    // 왼팔
+    glVertex3f(-0.3, -0.5, 0.0);   // 왼쪽 허리
+    glVertex3f(-0.5, -1, 0.0);     // 왼발
+    glVertex3f(0.0, -0.5, 0.0);
+    glVertex3f(0.5, -1, 0.0);      // 오른발
+    glVertex3f(0.3, -0.5, 0.0);    // 오른 허리
+    glVertex3f(0.9, 0.5, 0.0);     // 오른팔
+    
     glEnd();
     glFlush();
 }
@@ -17,7 +22,4 @@ int main(int argc, char* argv[]){
     glutDisplayFunc(mydisplay);
     glutMainLoop();
     return 0;
-    
-    
-    
 }
